27db39ae627e6fffe4991479df1adc2bfc4a29f5
Sara Asadi <45791009+saraasadi7899@users.noreply.github.com>
2021-07-05 20:03:53 +0430

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,7 +1,10 @@
-select * from users;
 select count(*) from users;
 explain select username from users;
 create index idx_username on users (username);
 show indexes in users;
 analyze TABLE users;
+explain select name from video;
+create index idx_name on video (name);
+show indexes in video;
+analyze TABLE video;
 
